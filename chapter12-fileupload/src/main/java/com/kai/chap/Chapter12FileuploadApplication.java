0e719e29095ad1c3504d7e53d92b1ca3b839034f
hok <1269893661@qq.com>
2019-03-25 18:13:23 +0800

    springboot在jsp页面实现三种不同方式的文件上传

---
+++ chapter12-fileupload/src/main/java/com/kai/chap/Chapter12FileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.kai.chap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter12FileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter12FileuploadApplication.class, args);
+    }
+
+}
