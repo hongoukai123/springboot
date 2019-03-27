64d2f18ae40efa179375c14ae006fdd451ecca15
hok <1269893661@qq.com>
2019-03-27 17:44:02 +0800

    springboot中springMVC里的@ResponseBody转化json流程、重定向、session、控制器通知、获取请求头信息

---
+++ chapter12-else/src/main/java/com/kai/chap/Chapter12ElseApplication.java
@@ -0,0 +1,13 @@
+package com.kai.chap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter12ElseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter12ElseApplication.class, args);
+    }
+
+}
